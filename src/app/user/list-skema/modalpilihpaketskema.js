93c4f174449f01e7031f73846fc8abf53409a785
bellawatsonf <bellawatsonf@gmail.com>
2023-08-31 20:06:33 +0700

    benerin modal pilih skema

--- src/app/user/list-skema/modalpilihpaketskema.js
+++ src/app/user/list-skema/modalpilihpaketskema.js
@@ -155,7 +155,19 @@ export default function ModalPilihPaketSkema(props) {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: { xs: "400px", md: "500px" },
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
           <div
             style={{
               display: "flex",
