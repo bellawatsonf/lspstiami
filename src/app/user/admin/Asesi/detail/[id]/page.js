4a5fd4183387110173a612544580ee08c7d481cf
bellawatsonf <bellawatsonf@gmail.com>
2023-09-20 21:31:51 +0700

    edit tampil ttd_Asesi

--- src/app/user/admin/Asesi/detail/[id]/page.js
+++ src/app/user/admin/Asesi/detail/[id]/page.js
@@ -528,7 +528,8 @@ export default function DetailAsesi_Pendaftaran() {
                 <img
                   // src={`${ttdAsesi}`}
                   src={
-                    asesiSkemaById?.asesi?.ttd_asesi !== null
+                    asesiSkemaById?.asesi?.ttd_asesi !== null ||
+                    asesiSkemaById?.asesi?.ttd_asesi !== undefined
                       ? `/api/download-asesi/${
                           asesiSkemaById?.asesi?.ttd_asesi.split("/")[4]
                         }`
