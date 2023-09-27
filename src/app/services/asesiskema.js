cfd5abde4524c899ce39d481b00ebbb24d1fd0e4
bellawatsonf <bellawatsonf@gmail.com>
2023-09-27 15:08:06 +0700

    tab asesiskema

--- src/app/services/asesiskema.js
+++ src/app/services/asesiskema.js
@@ -7,9 +7,9 @@ export function fetchAsesiSkemaServices(param) {
     dispatch(Loading(true));
 
     axios({
-      url: `/api/asesi-skema?page=${param.page - 1}&size=1&statusCek=${
-        param.statusCek
-      }`,
+      url: `/api/asesi-skema?page=${param.page - 1}&size=${
+        param.size
+      }&statusCek=${param.statusCek}`,
       method: "GET",
     })
       .then((data) => {
