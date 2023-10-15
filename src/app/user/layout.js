58a3511402c772482a27fc09e7d82d51978b1a3a
bellawatsonf <bellawatsonf@gmail.com>
2023-10-15 20:40:57 +0700

    edit layout

--- src/app/user/layout.js
+++ src/app/user/layout.js
@@ -9,6 +9,109 @@ import HeaderDashboard from "./component/header";
 import Sidebar from "./component/sidebar/index2";
 import Login from "../login/page";
 // import jsHttpCookie from "cookie";
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import Person3Icon from "@mui/icons-material/Person3";
+import Modal from "@mui/material/Modal";
+import LogoutIcon from "@mui/icons-material/Logout";
+import sidebarStyle from "./component/sidebar/sidebarstyle.module.css";
+
+const drawerWidth = 240;
+
+const style = {
+  position: "absolute",
+  top: "10%",
+  right: "1%",
+  // transform: "translate(-50%, -50%)",
+  width: 200,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,6 +138,26 @@ const inter = Inter({ subsets: ["latin"] });
 // };
 
 export default function DashboardLayout({ children }) {
+  const [openM, setOpenM] = React.useState(false);
+  const handleOpen = () => setOpenM(true);
+  const handleClose = () => setOpenM(false);
+
+  function handleLogout() {
+    console.log("handleLogout");
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("user");
+    router.push("/login");
+  }
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   console.log("masuk layour dashboard");
   let router = useRouter();
   let token;
@@ -59,20 +182,187 @@ export default function DashboardLayout({ children }) {
     setToken(sessionStorage.getItem("token"));
   }, []);
 
+  let menuSidebar = [
+    {
+      name: "Status",
+      url: "/user/dashboard",
+      role: "asesi",
+    },
+    {
+      name: "Data Asesi",
+      url: "/user/profiluser",
+      role: "asesi",
+    },
+    {
+      name: "User",
+      url: "/user/admin/admin",
+      role: "admin",
+    },
+  ];
   return (
     <Fragment>
       {(tokens && users.role === "asesi") ||
       (tokens && users.role === "admin") ? (
-        // <PDFViewer>
-        <div className={inter.className}>
-          <div className={`${styleDashboard.boxContainer}`}>
-            <Sidebar />
-            <div className={`${styleDashboard.boxContent}`}>
-              <HeaderDashboard />
-              <div className={`${styleDashboard.content}`}>{children}</div>
-            </div>
-          </div>
-        </div>
+        // <div className={inter.className}>
+        //   <div className={`${styleDashboard.boxContainer}`}>
+        //     <Sidebar />
+        //     <div className={`${styleDashboard.boxContent}`}>
+        //       <HeaderDashboard />
+        //       <div className={`${styleDashboard.content}`}>{children}</div>
+        //     </div>
+        //   </div>
+        // </div>
+        <Box sx={{ display: "flex" }}>
+          <CssBaseline />
+          <AppBar position="fixed" open={open}>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                edge="start"
+                sx={{
+                  marginRight: 5,
+                  ...(open && { display: "none" }),
+                }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  padding: "10px",
+                  width: "100%",
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontWeight: 600,
+                    fontSize: { xs: "12px", md: "15px" },
+                    paddingTop: "10px",
+                  }}
+                >
+                  Halo, {user.nama}
+                </Typography>
+                <Person3Icon
+                  onClick={handleOpen}
+                  sx={{
+                    fontSize: { xs: "40px", md: "50px" },
+                    cursor: "pointer",
+                  }}
+                />
+              </div>
+              <Modal
+                open={openM}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                  <Typography
+                    id="modal-modal-title"
+                    sx={{ fontSize: "17px", cursor: "pointer" }}
+                    onClick={() => {
+                      handleLogout();
+                    }}
+                  >
+                    <LogoutIcon sx={{ fontSize: "17px" }} />
+                    {"  "}Logout
+                  </Typography>
+                </Box>
+              </Modal>
+            </Toolbar>
+          </AppBar>
+          <Drawer variant="permanent" open={open}>
+            <DrawerHeader>
+              <div className={`${sidebarStyle.boxlogo}`}>
+                <img
+                  className={`${sidebarStyle.imglogo}`}
+                  style={{ cursor: "pointer" }}
+                  src="/logoolsp.png"
+                  onClick={() => router.push("/")}
+                />
+              </div>
+              <IconButton onClick={handleDrawerClose}>
+                {theme.direction === "rtl" ? (
+                  <ChevronRightIcon />
+                ) : (
+                  <ChevronLeftIcon />
+                )}
+              </IconButton>
+            </DrawerHeader>
+            <Divider />
+
+            <List>
+              {menuSidebar
+                .filter((dt) => dt.role === user.role)
+                .map((text, index) => (
+                  <ListItem
+                    key={index}
+                    disablePadding
+                    sx={{ display: "block" }}
+                  >
+                    <ListItemButton
+                      sx={{
+                        minHeight: 48,
+                        justifyContent: open ? "initial" : "center",
+                        px: 2.5,
+                      }}
+                    >
+                      <ListItemIcon
+                        sx={{
+                          minWidth: 0,
+                          mr: open ? 3 : "auto",
+                          justifyContent: "center",
+                        }}
+                        onClick={() => router.push(text.url)}
+                      >
+                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        {/* {text.name} */}
+                      </ListItemIcon>
+                      <ListItemText
+                        primary={text.name}
+                        sx={{ opacity: open ? 1 : 0 }}
+                      />
+                    </ListItemButton>
+                  </ListItem>
+                ))}
+            </List>
+            <Divider />
+            {/* <List>
+              {["All mail", "Trash", "Spam"].map((text, index) => (
+                <ListItem key={text} disablePadding sx={{ display: "block" }}>
+                  <ListItemButton
+                    sx={{
+                      minHeight: 48,
+                      justifyContent: open ? "initial" : "center",
+                      px: 2.5,
+                    }}
+                  >
+                    <ListItemIcon
+                      sx={{
+                        minWidth: 0,
+                        mr: open ? 3 : "auto",
+                        justifyContent: "center",
+                      }}
+                    >
+                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={text}
+                      sx={{ opacity: open ? 1 : 0 }}
+                    />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List> */}
+          </Drawer>
+          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+            <DrawerHeader />
+            <div className={`${styleDashboard.content}`}>{children}</div>
+          </Box>
+        </Box>
       ) : (
         // {/* </PDFViewer> */}
         // router.push("/login")
